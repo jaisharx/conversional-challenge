e075d661e468c4cdd8659b0a24423fd9bfe34562
jai-sharma <sendtojsharma@gmail.com>
2021-07-26 19:19:05 +0530

    switching to jsx

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import { Heading } from '@chakra-ui/react';
-
-export default function Home() {
-    return (
-        <>
-            <Heading>Hello, World!</Heading>
-        </>
-    );
-}
---
+++ pages/index.jsx
@@ -0,0 +1,5 @@
+import { Heading } from '@chakra-ui/react';
+
+export default function HomePage() {
+    return <Heading>Hello, World!</Heading>;
+}
