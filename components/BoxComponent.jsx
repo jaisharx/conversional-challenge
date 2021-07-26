e8bb56224a2dca3d6eb38e10adfb3e17443e3b6e
jai-sharma <sendtojsharma@gmail.com>
2021-07-26 18:29:39 +0530

    adding basic set of components

---
+++ components/BoxComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BoxComponent() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ components/ParagraphComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ParagraphComponent() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
