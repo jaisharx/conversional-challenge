455a0888c234732a1595af9144891371ad102943
jai-sharma <sendtojsharma@gmail.com>
2021-07-26 18:50:52 +0530

    adding modal component basic structure

--- components/ModalComponent.jsx
+++ components/ModalComponent.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Button,
+} from '@chakra-ui/react';
+import { LoremIpsum } from 'react-lorem-ipsum';
 
-export default function ModalComponent() {
+export default function ModalComponent({ isOpen, onClose }) {
     return (
-        <div>
-            
-        </div>
-    )
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>Modal Title</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                    <LoremIpsum p={1} />
+                </ModalBody>
+
+                <ModalFooter>
+                    <Button colorScheme="blue" mr={3} onClick={onClose}>
+                        Close
+                    </Button>
+                    <Button variant="ghost">Secondary Action</Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+    );
 }
