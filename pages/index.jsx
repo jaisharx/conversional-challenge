df28e7952e205ad2f61225da0b0a6c20961a83ae
jai-sharma <sendtojsharma@gmail.com>
2021-07-26 23:04:09 +0530

    adding navbar

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,10 @@
 import Example1 from 'data/data.json';
 import Example2 from 'data/data2.json';
 
+import { Container } from '@chakra-ui/react';
+import { useState } from 'react';
+import Navbar from 'components/navbar';
+
 import ModalComponent from 'components/ModalComponent';
 import BoxComponent from 'components/BoxComponent';
 import ButtonComponent from 'components/ButtonComponent';
@@ -10,9 +14,15 @@ import LinkComponent from 'components/LinkComponent';
 import ListComponent from 'components/ListComponent';
 import ParagraphComponent from 'components/ParagraphComponent';
 import traverseData from 'utils/traverseData';
-import { Container, Heading, HStack } from '@chakra-ui/react';
+
+function getExampleDataFromKey(key, allExamples) {
+    if (key === 'Example1') return allExamples[0];
+    if (key === 'Example2') return allExamples[1];
+}
 
 export default function HomePage() {
+    const [selectedExample, setSelectedExample] = useState('Example1');
+
     const allComps = [
         ModalComponent,
         BoxComponent,
@@ -24,10 +34,19 @@ export default function HomePage() {
         ParagraphComponent,
     ];
 
+    const allExamples = [Example1, Example2];
+
     return (
         <>
-            <Container maxW="container.lg">
-                {traverseData(Example1, allComps)}
+            <Navbar
+                selectedExample={selectedExample}
+                setSelectedExample={setSelectedExample}
+            />
+            <Container maxW="container.lg" mt="4">
+                {traverseData(
+                    getExampleDataFromKey(selectedExample, allExamples),
+                    allComps
+                )}
             </Container>
         </>
     );
