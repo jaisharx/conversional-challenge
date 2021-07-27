812ba4d4674572f5d87ccbe0295fc1c15c516491
jai-sharma <sendtojsharma@gmail.com>
2021-07-27 05:40:27 +0530

    moving modal isOpen state into a context

---
+++ contexts/modal-context.jsx
@@ -0,0 +1,25 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+
+const ModalContext = createContext();
+
+function ModalProvider({ children }) {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    return (
+        <ModalContext.Provider value={{ isModalOpen, setIsModalOpen }}>
+            {children}
+        </ModalContext.Provider>
+    );
+}
+
+function useModal(initalModalState) {
+    const { isModalOpen, setIsModalOpen } = useContext(ModalContext);
+
+    useEffect(() => {
+        setIsModalOpen(initalModalState);
+    }, []);
+
+    return { isModalOpen, setIsModalOpen };
+}
+
+export { ModalProvider, useModal };
