a072382314db569c3b82e24ac3bcc1b3f30ded56
jai-sharma <sendtojsharma@gmail.com>
2021-07-27 05:54:20 +0530

    adding conditional modal open logic based on doesOpenModal prop

--- primitives/LinkComponent.jsx
+++ primitives/LinkComponent.jsx
@@ -1,10 +1,20 @@
 import { HStack, Link } from '@chakra-ui/react';
 import { BiLinkExternal } from 'react-icons/bi';
+import { useModal } from 'contexts/modal-context';
 
-export default function LinkComponent({ url, text }) {
+export default function LinkComponent({ url, text, doesOpenModal }) {
+    const { setIsModalOpen } = useModal();
+
+    const onLinkClick = () => {
+        if (doesOpenModal) {
+            setIsModalOpen(true);
+            console.log('Modal Opened!');
+        }
+    };
+
     return (
         <HStack>
-            <Link href={url} isExternal>
+            <Link href={url} isExternal onClick={onLinkClick}>
                 {text}
             </Link>
             <BiLinkExternal />
