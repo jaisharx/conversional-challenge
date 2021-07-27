3f8285220f60292364f253fb001768db558a9b95
jai-sharma <sendtojsharma@gmail.com>
2021-07-27 05:49:38 +0530

    adding conditional logic based on doesOpenModal prop for button

--- primitives/ButtonComponent.jsx
+++ primitives/ButtonComponent.jsx
@@ -1,11 +1,20 @@
 import { Button } from '@chakra-ui/react';
+import { useModal } from 'contexts/modal-context';
 
-export default function ButtonComponent({ text }) {
+export default function ButtonComponent({ text, doesOpenModal }) {
+    const { setIsModalOpen } = useModal();
+
+    const onButtonClick = () => {
+        if (doesOpenModal) {
+            setIsModalOpen(true);
+            console.log('Modal Opened!');
+            return;
+        }
+        console.log('Button Clicked!');
+    };
+
     return (
-        <Button
-            colorScheme="green"
-            onClick={() => console.log('Button Clicked!')}
-        >
+        <Button colorScheme="green" onClick={onButtonClick}>
             {text}
         </Button>
     );
