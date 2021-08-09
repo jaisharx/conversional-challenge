b385eb9aaae1791105e71adac2c8ac4e613981e6
jai-sharma <sendtojsharma@gmail.com>
2021-08-09 11:03:43 +0530

    adding modal component

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,29 +1,36 @@
-import { Box, Button, Center, Heading, useDisclosure } from '@chakra-ui/react';
-import ModalComponent from 'primitives/ModalComponent';
-import BoxComponent from 'primitives/BoxComponent';
-import ButtonComponent from 'primitives/ButtonComponent';
-import H1Component from 'primitives/H1Component';
-import H2Component from 'primitives/H2Component';
-import LinkComponent from 'primitives/LinkComponent';
-import ListComponent from 'primitives/ListComponent';
-import ParagraphComponent from 'primitives/ParagraphComponent';
+import { Box } from '@chakra-ui/react'
+import ModalComponent from 'primitives/ModalComponent'
+import BoxComponent from 'primitives/BoxComponent'
+import ButtonComponent from 'primitives/ButtonComponent'
+import H1Component from 'primitives/H1Component'
+import H2Component from 'primitives/H2Component'
+import LinkComponent from 'primitives/LinkComponent'
+import ListComponent from 'primitives/ListComponent'
+import ParagraphComponent from 'primitives/ParagraphComponent'
 
 export default function TestPage() {
-    return (
-        <Box maxW="container.md" mx="auto">
-            <BoxComponent borderSize="1px">
-                <H1Component text="H1 Heading" />
-                <H2Component text="H2 Heading" />
-                <ParagraphComponent text="P paragraph" />
-                <ListComponent
-                    li={['free to use', 'superfast', 'and pretty, too!']}
-                />
-                <ButtonComponent text="Click me to open the modal" />
-                <LinkComponent
-                    url="https://lmgtfy.com/"
-                    text="I open a link but I should also open the modal."
-                />
-            </BoxComponent>
-        </Box>
-    );
+  return (
+    <Box maxW="container.md" mx="auto">
+      <BoxComponent borderSize="1px">
+        <H1Component text="H1 Heading" />
+        <H2Component text="H2 Heading" />
+        <ParagraphComponent text="P paragraph" />
+        <ListComponent li={['free to use', 'superfast', 'and pretty, too!']} />
+        <ButtonComponent
+          doesOpenModal={true}
+          text="Click me to open the modal"
+        />
+        <LinkComponent
+          doesOpenModal={false}
+          url="https://lmgtfy.com/"
+          text="I open a link but I should also open the modal."
+        />
+        <ModalComponent isOpen={true}>
+          <H1Component text="H1 Heading" />
+          <H2Component text="H2 Heading" />
+          <ParagraphComponent text="P paragraph" />
+        </ModalComponent>
+      </BoxComponent>
+    </Box>
+  )
 }
